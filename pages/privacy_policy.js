66c49d93eb5bd027afde4edc44d3a73d50d24e04
Joris <Neyens01@gmail.com>
2022-05-24 21:12:47 +0200

    first commit

---
+++ pages/privacy_policy.js
@@ -0,0 +1,258 @@
+import React from 'react';
+
+export default function privacy_policy() {
+    return (
+      <main>
+        <div className="container pb-5">
+          <div className="col-12 col-md-6 col-lg-6">
+            <section>
+              <h1 className="mt-5 pt-5">The Send Prayer - Privacy Policy </h1>
+              <h2>Privacy Policy</h2>
+              <p>
+                The Send Prayer is a non-profit project and can be used free and commercial free. The person responsible for processing the personal data we
+                process is the owner of Neyens, a company operating under the name Brandbrewery. Our contact information is:
+              </p>
+              <div className="d-flex">
+                <b>Address: </b>
+                <p>&nbsp; Kna-veien 50, 4887, Grimstad</p>
+              </div>
+              <div className="d-flex">
+                <b>Name: </b>
+                <p>&nbsp; info@brandbrewery.no</p>
+              </div>
+              <div className="d-flex">
+                <b>Org.nr.: </b>
+                <p>&nbsp; 927 506 041</p>
+              </div>
+              <br></br>
+              <b>Data controller and owner</b>
+              <div className="d-flex">
+                <b>Name: </b>
+                <p>&nbsp;Joris Neyens</p>
+              </div>
+              <div className="d-flex my-0">
+                <b>Email:</b>
+                <p>&nbsp; info@brandbrewery.no</p>
+              </div>
+              <div>
+                <p className="pt-4">
+                  This app is developed by Neyens based in Norway (org. No. 927 506 041) in collaboration with The Send Prayer – a working group connected to
+                  The Send responsible for this follow up program. Neyens operates under the name Brandbrewery. as a free follow up of The Send. Through this
+                  app we seek to guide the participants into a living, sustainable prayer life.
+                </p>
+                <br></br>
+                <p>
+                  The App provides weekly podcast introduced by a blog post. Users can post comments on the podcasts to create further interaction. Posts and
+                  podcasts can be downloaded to be read or listened on a later moment. In addition, users can find recourses to help their devotional life,
+                  relevant events, and locations where they can meet others who are seeking to grow in their relationship with God. We use push notifications to
+                  inform people about new content. People are free to turn off these notifications. The app uses geolocations to invite people though push
+                  notifications to events close to them, this is not mandatory. People are asked to sign in with their email address, this gives us the
+                  possibility to know who are actively using the app. It gives us the possibility to adapt our content and push notification to this active
+                  group. This is not mandatory.
+                </p>
+              </div>
+            </section>
+            <section className="pt-5">
+              <div>
+                <h2>Collection of personal information and location data </h2>
+                <p>Among the types of Personal Data that this Application collects, by itself or through third parties, there are: Cookie and Usage Data.</p>
+                <br></br>
+                <b>Sign up and profile</b>
+                <p>
+                  Users are asked to sign in using their email address and a password. When making a profile users can choose to add location, description,
+                  profile picture. This personal information is not mandatory to use the app.{" "}
+                </p>
+                <br></br>
+                <b>Content commenting</b>
+                <p>
+                  Depending on the settings chosen by the Owner, Users may also leave anonymous comments. If there is an email address among the Personal Data
+                  provided by the User, it may be used to send notifications of comments on the same content. Users are responsible for the content of their own
+                  comments.{" "}
+                </p>
+                <br></br>
+                <b>location data </b>
+                <p>Users are asked to allow geolocation to run on the background also when the app is not in use, this is not mandatory.</p>
+                <br></br>
+                <b>About the personal data</b>
+                <p>The Personal Data may be freely provided by the User or collected automatically when using this Application.</p>
+                <br></br>
+                <b>Cookies and other tracking tools</b>
+                <p>
+                  Any use of Cookies - or of other tracking tools - by this Application or by the owners of third-party services used by this Application,
+                  unless stated otherwise, serves to identify Users, and remember their preferences, for the sole purpose of providing the service required by
+                  the User. Failure to provide certain Personal Data may make it impossible for this Application to provide all of its services.
+                </p>
+              </div>
+            </section>
+            <section className="pt-5">
+              <h2>Why data is gathered</h2>
+              <br></br>
+              <b>Sign up and profile</b>
+              <p>
+                Personal data, email address, location and description help us to provide relevant content and a better understanding of our target group. This
+                is all aimed at improving the user’s experience.
+              </p>
+              <br></br>
+              <b>Content commenting</b>
+              <p>
+                A username and email address needed to place a comment. This, to make it possible to respond to comments and in this way improve the user’s
+                experience.
+              </p>
+              <br></br>
+              <b>location data </b>
+              <p>Geolocation is used to send invitations to events in the form of a push notification to people who are close to the event.</p>
+            </section>
+            <section className="pt-5">
+              <h2>Mode and place of processing the Data</h2>
+              <br></br>
+              <b>Methods of processing</b>
+              <p>
+                The Data Controller processes the Data of Users in a proper manner and shall take appropriate security measures to prevent unauthorized access,
+                disclosure, modification, or unauthorized destruction of the Data.
+              </p>
+              <br></br>
+              <p>
+                The Data processing is carried out using computers and/or IT enabled tools, following organizational procedures and modes strictly related to
+                the purposes indicated. In addition to the Data Controller, in some cases, the Data may be accessible to certain types of persons in charge,
+                involved with the operation of the site (administration, sales, marketing, legal, system administration) or external parties (such as
+                third-party technical service providers, mail carriers, hosting providers, IT companies, communications agencies) appointed, if necessary, as
+                Data Processors by the Owner. The updated list of these parties may be requested from the Data Controller at any time.
+              </p>
+              <br></br>
+              <b>Place</b>
+              <p>
+                The Data is processed at the Data Controller's operating offices and in any other places where the parties involved with the processing are
+                located. For further information, please contact the Data Controller.
+              </p>
+              <br></br>
+              <b>Retention time</b>
+              <p>
+                The Data is kept for the time necessary to provide the service requested by the User, or stated by the purposes outlined in this document, and
+                the User can always request that the Data Controller suspend or remove the data.
+              </p>
+              <br></br>
+              <b>The use of the collected Data</b>
+              <p>
+                The Data concerning the User is collected to allow the Application to provide its services, as well as for the following purposes: Access to
+                third party services' accounts, Creation of the user in app profile, Content commenting and Interaction with external social networks and
+                platforms. The Personal Data used for each purpose is outlined in the specific sections of this document.
+              </p>
+            </section>
+            <section className="pt-5">
+              <h2>Additional information about Data collection and processing</h2>
+              <br></br>
+              <b>Legal action</b>
+              <p>
+                The User's Personal Data may be used for legal purposes by the Data Controller, in Court or in the stages leading to possible legal action
+                arising from improper use of this Application or the related services. The User is aware of the fact that the Data Controller may be required to
+                reveal personal data upon request of public authorities.
+              </p>
+              <br></br>
+              <b>Additional information about User's Personal Data</b>
+              <p>
+                In addition to the information contained in this privacy policy, this Application may provide the User with additional and contextual
+                information concerning particular services or the collection and processing of Personal Data upon request.
+              </p>
+              <br></br>
+              <b>System Logs and Maintenance</b>
+              <p>
+                For operation and maintenance purposes, this Application and any third party services may collect files that record interaction with this
+                Application (System Logs) or use for this purpose other Personal Data (such as IP Address).
+              </p>
+              <br></br>
+              <b>Information not contained in this policy</b>
+              <p>
+                More details concerning the collection or processing of Personal Data may be requested from the Data Controller at any time. Please see the
+                contact information at the beginning of this document.
+              </p>
+              <br></br>
+              <b>The rights of Users</b>
+              <p>
+                Users have the right, at any time, to know whether their Personal Data has been stored and can consult the Data Controller to learn about their
+                contents and origin, to verify their accuracy or to ask for them to be supplemented, cancelled, updated or corrected, or for their
+                transformation into anonymous format or to block any data held in violation of the law, as well as to oppose their treatment for any and all
+                legitimate reasons. Requests should be sent to the Data Controller at the contact information set out above.
+              </p>
+              <br></br>
+              <b>This Application does not support “Do Not Track” requests.</b>
+              <p>To determine whether any of the third party services it uses honor the “Do Not Track” requests, please read their privacy policies.</p>
+              <br></br>
+              <b>Changes to this privacy policy</b>
+              <p>
+                The Data Controller reserves the right to make changes to this privacy policy at any time by giving notice to its Users on this page. It is
+                strongly recommended to check this page often, referring to the date of the last modification listed at the bottom. If a User objects to any of
+                the changes to the Policy, the User must cease using this Application and can request that the Data Controller erase the Personal Data. Unless
+                stated otherwise, the then-current privacy policy applies to all Personal Data the Data Controller has about Users.
+              </p>
+              <br></br>
+              <b>Information from the use of our applications </b>
+              <p>
+                When you use our mobile apps, we may collect certain information in addition to information described elsewhere in this Policy. For example, we
+                may collect information about the type of device and operating system you use. We may ask you if you want to receive push notifications about
+                activity in your account. If you have opted into these notifications and no longer want to receive them, you may turn them off through your
+                operating system. We may ask for, access or track location-based information from your mobile device so that you can test location-based
+                features offered by the Services or to receive targeted push notifications based on your location. If you have opted in to share those
+                location-based information, and no longer want to share them, you may turn sharing off through your operating system. We may use mobile
+                analytics software (such as crashlytics.com) to better understand how people use our application. We may collect information about how often you
+                use the application and other performance data.
+              </p>
+            </section>
+            <section className="pt-5 pb-5">
+              <h2>Definitions and legal references</h2>
+              <br></br>
+              <b>Personal Data (or Data)</b>
+              <p>
+                Any information regarding a natural person, a legal person, an institution, or an association, which is, or can be, identified, even indirectly,
+                by reference to any other information, including a personal identification number.
+              </p>
+              <br></br>
+              <b>Usage Data</b>
+              <p>
+                Information collected automatically from this Application (or third party services employed in this Application ), which can include: the IP
+                addresses or domain names of the computers utilized by the Users who use this Application, the URI addresses (Uniform Resource Identifier), the
+                time of the request, the method utilized to submit the request to the server, the size of the file received in response, the numerical code
+                indicating the status of the server's answer (successful outcome, error, etc.), the country of origin, the features of the browser and the
+                operating system utilized by the User, the various time details per visit (e.g., the time spent on each page within the Application) and the
+                details about the path followed within the Application with special reference to the sequence of pages visited, and other parameters about the
+                device operating system and/or the User's IT environment.
+              </p>
+              <br></br>
+              <b>User</b>
+              <p>The individual using this Application, which must coincide with or be authorized by the Data Subject, to whom the Personal Data refer.</p>
+              <br></br>
+              <b>Data Subject</b>
+              <p>The legal or natural person to whom the Personal Data refers to.</p>
+              <br></br>
+              <b>Data Processor (or Data Supervisor)</b>
+              <p>
+                The natural person, legal person, public administration or any other body, association or organization authorized by the Data Controller to
+                process the Personal Data in compliance with this privacy policy.
+              </p>
+              <br></br>
+              <b>Data Controller (or Owner)</b>
+              <p>
+                The natural person, legal person, public administration or any other body, association or organization with the right, also jointly with another
+                Data Controller, to make decisions regarding the purposes, and the methods of processing of Personal Data and the means used, including the
+                security measures concerning the operation and use of this Application. The Data Controller, unless otherwise specified, is the Owner of this
+                Application.
+              </p>
+              <br></br>
+              <b>This Application</b>
+              <p>The hardware or software tool by which the Personal Data of the User is collected.</p>
+              <br></br>
+              <b>Cookie</b>
+              <p>Small piece of data stored in the User's device.</p>
+              <br></br>
+              <b>Legal information</b>
+              <p>
+                Notice to European Users: this privacy statement has been prepared in fulfillment of the obligations under Art. 10 of EC Directive n. 95/46/EC,
+                and under the provisions of Directive 2002/58/EC, as revised by Directive 2009/136/EC, on the subject of Cookies.
+              </p>
+              <br></br>
+              <p>This privacy policy relates solely to this Application.</p>
+            </section>
+          </div>
+        </div>
+      </main>
+    );
+}
